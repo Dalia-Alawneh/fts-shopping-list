5ffe7ce1ba3919f57866b433f77726fc110d6021
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-14 15:43:04 +0300

    Create `Navbar` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Router, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home.jsx"
+import Navbar from "./components/Navbar/Navbar.jsx"
 
 function App() {
 
   return (
-    <></>
+    <>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {
+  Navbar as Nav,
+  NavbarBrand,
+  NavbarCollapse, NavbarToggle,
+  TextInput
+} from "flowbite-react";
+import logo from '#assets/logo.svg';
+import { NavLink } from "react-router-dom";
+import { customInputTheme } from '../../theme/index.js';
+
+const Navbar = () => {
+
+  return (
+    <Nav rounded>
+      <NavbarBrand href="/">
+        <img src={logo} className="h-12 sm:h-16" alt="Logo" />
+      </NavbarBrand>
+
+      <div className="order-3 w-full sm:order-2 mt-5 sm:mt-0 sm:w-auto sm:flex-1 sm:mx-4 flex justify-center items-center">
+        <TextInput
+          id="search"
+          type="search"
+          placeholder="What are you rooting for?"
+          className="w-full sm:w-[300px] lg:w-[400px] focus:shadow-none focus:border-primary-green"
+          theme={customInputTheme}
+        />
+      </div>
+
+      <NavbarToggle className="order-2 sm:order-3" />
+
+      <NavbarCollapse className="order-2 sm:order-3" >
+        <NavLink to='/' className={({ isActive }) =>
+          isActive
+            ? "text-primary-green font-bold hover:text-primary-green"
+            : "text-gray-700 hover:text-primary-green"
+        }>Home</NavLink>
+        <NavLink to='/cart' className={({ isActive }) =>
+          isActive
+            ? "text-primary-green font-bold hover:text-primary-green"
+            : "text-gray-700 hover:text-primary-green"
+        }>Cart</NavLink>
+      </NavbarCollapse>
+    </Nav>
+  );
+};
+
+export default Navbar
