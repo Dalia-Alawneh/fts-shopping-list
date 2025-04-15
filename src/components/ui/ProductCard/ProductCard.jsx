c7e5368ad0e0f968b99bdd17f0b8650501ec7acc
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-15 06:37:12 +0300

    Create `ProductCard`

---
+++ src/components/ui/ProductCard/ProductCard.jsx
@@ -0,0 +1,22 @@
+import { ShoppingBag, ShoppingCart } from 'lucide-react';
+import React from 'react'
+
+const ProductCard = ({ product }) => {
+  const { image, name, price, rating } = product;
+  return (
+    <div className='shadow bg-gray rounded max-h-[300px] border border-gray'>
+      <img src={image} className='h-full m-auto' alt={name} />
+      <div className='p-2 py-5 bg-white border border-gray'>
+        <div className="flex justify-between items-center">
+          <div>
+            <h3 className='text-primary-black font-bold mb-2'>{name}</h3>
+            <span>${price}</span>
+          </div>
+          <button><ShoppingCart size={32} className='text-primary-black' /></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/components/ui/TabBar/TabBar.jsx
+++ src/components/ui/TabBar/TabBar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { categories, products } from './../../constants/index.js';
 import TabBarItem from './../TabItem/TabItem.jsx';
+import ProductCard from '../ProductCard/ProductCard.jsx';
 
 export default function TabBar() {
   const [activeTab, setActiveTab] = useState(categories[0]);
@@ -24,9 +25,7 @@ export default function TabBar() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {filteredProducts.map((product) => (
-          <div>
-            {product.name}
-          </div>
+          <ProductCard product={product}/>
         ))}
       </div>
     </div>
