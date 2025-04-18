fa20eb99195346aefe2a5d667d3cc53ac18f780a
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-18 11:46:46 +0300

    Edit total price text design

--- src/pages/Cart/components/CartItemRow/CartItemRow.jsx
+++ src/pages/Cart/components/CartItemRow/CartItemRow.jsx
@@ -1,12 +1,12 @@
 const CartItemRow = ({ item }) => (
   <tr className="text-center border-t border-gray-200">
     <td className="p-2">
-      <img className="w-16 m-auto" src={item.image} alt={item.name} />
+      <img className="h-20 m-auto" src={item.image} alt={item.name} />
     </td>
     <td className="p-2">{item.name}</td>
     <td className="p-2">${item.price}</td>
     <td className="p-2">{item.quantity}</td>
-    <td className="p-2">${item.price * item.quantity}</td>
+    <td className="p-2 font-bold text-primary-black">${item.price * item.quantity}</td>
   </tr>
 );
 
