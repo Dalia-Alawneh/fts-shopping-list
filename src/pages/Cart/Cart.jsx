32e8f9bd245c289eae2dcf0e3b69ccd244ebb6bf
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-22 06:38:27 +0300

    Refctor cartitems component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react';
 import PageHeader from '../../Components/shared/PageHeader'
 import CartItems from './Components/CartItems'
+import { CartContext } from '../../Context/CartContext.jsx';
+import CartTotal from './Components/CartTotal/CartTotal.jsx';
 
 const Cart = () => {
+  const { state, dispatch } = useContext(CartContext);
+  const { cartItems } = state;
   return (
     <>
       <PageHeader title="Shopping Cart" />
-      <CartItems />
+      <CartItems cartItems={cartItems} dispatch={dispatch} />
+      <CartTotal cartItems={cartItems} />
     </>
   )
 }
--- src/pages/Cart/components/CartItems/CartItems.jsx
+++ src/pages/Cart/components/CartItems/CartItems.jsx
@@ -1,14 +1,9 @@
-import React, { useContext, useState } from 'react'
-import { CartContext } from '../../../../Context/CartContext.jsx';
+import { useState } from 'react'
 import Modal from '#Components/shared/Modal';
 import CartItemsTable from '../CartItemsTable';
-import { Link } from 'react-router-dom';
 import toast from 'react-hot-toast';
-const CartItems = () => {
-  const { state } = useContext(CartContext);
-  const { cartItems } = state;
+const CartItems = ({cartItems, dispatch}) => {
   let [isOpen, setIsOpen] = useState(false)
-  const { dispatch } = useContext(CartContext);
   const [selectedId, setSelectedId] = useState(null);
 
   function openConfirmModal(id) {
@@ -36,10 +31,7 @@ const CartItems = () => {
   const handleDecrement = (id) => {
     dispatch({ type: "DECREMENT_QUANTITY", payload: { id } });
   };
-  const totalAmount = cartItems.reduce((accum, item) =>
-    accum + (item.price * item.quantity), 0);
-  const totalCount = cartItems.reduce((accum, item) =>
-    accum + item.quantity, 0);
+
   return (
     <div className="overflow-x-auto p-10">
       <CartItemsTable
@@ -48,23 +40,6 @@ const CartItems = () => {
         onIncrement={handleIncrement}
         onDecrement={handleDecrement}
       />
-      {cartItems?.length > 0 && (
-        <div className="flex justify-end mt-8 pe-0 lg:pe-10">
-          <div className="bg-white rounded-2xl shadow-md p-6 w-full max-w-sm border border-gray-200">
-            <div className="flex justify-between items-center mb-4">
-              <h4 className="text-lg font-semibold">Total Amount</h4>
-              <span className="text-primary-green font-bold text-xl">${totalAmount}</span>
-            </div>
-            <div className="flex justify-between items-center mb-4">
-              <h4 className="text-lg font-semibold">Number of Products</h4>
-              <span className="text-primary-green font-bold text-xl">{totalCount}</span>
-            </div>
-            <Link to='/checkout' className="block text-center text-lg w-full py-2 px-4 rounded-lg bg-primary-green text-white hover:bg-primary-green-dark transition duration-200">
-              Proceed to Checkout
-            </Link>
-          </div>
-        </div>
-      )}
       <Modal title={'Confirm Delete'}
         description={"Are you sure you want to delete this item? This action cannot be undone."}
         isOpen={isOpen}
