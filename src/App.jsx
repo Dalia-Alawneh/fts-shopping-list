e9452a4103eabdda202ad76b4d698744b500f56c
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-22 06:27:33 +0300

    Add notFound

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import { CartContext, CartProvider } from "./Context/CartContext.jsx"
 import Cart from "./Pages/Cart"
 import Checkout from "./Pages/Checkout"
 import { Toaster } from "react-hot-toast"
-import Footer from "./Components/shared/Footer/Footer.jsx";
+import NotFound from "./Pages/NotFound/NotFound.jsx";
+import Layout from "./Components/shared/Layout/Layout.jsx";
 
 function App() {
 
   return (
     <CartProvider>
-      <Navbar />
       <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/cart" Component={Cart} />
-        <Route path="/checkout" Component={Checkout} />
+        <Route path="/" Component={Layout}>
+          <Route index Component={Home} />
+          <Route path="cart" Component={Cart} />
+          <Route path="checkout" Component={Checkout} />
+        </Route>
+        <Route path="*" Component={NotFound} />
       </Routes>
-      <Footer/>
       <Toaster />
     </CartProvider>
   )
---
+++ src/router/index.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home";
+import Cart from "../Pages/Cart";
+import NotFound from "../Pages/NotFound";
+import Layout from "../Components/shared/Layout/Layout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children:[
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/cart",
+        Component: Cart,
+        children: [
+          {
+            path: "checkout",
+            Component: Cart,
+          },
+        ]
+      },
+    ]
+  },
+  {
+    path: '*',
+    Component: NotFound
+  }
+]);
+
+
+export default router;
