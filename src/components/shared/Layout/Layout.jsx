9042a4e43d04dcc2f4857d7611a9648640ecd8db
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-22 06:56:48 +0300

    Formatte

--- src/components/shared/Layout/Layout.jsx
+++ src/components/shared/Layout/Layout.jsx
@@ -6,11 +6,11 @@ import Footer from '../Footer'
 const Layout = () => {
   return (
     <>
-    <Navbar/>
-    <main className='flex-1'>
-      <Outlet/>
-    </main>
-    <Footer/>
+      <Navbar />
+      <main className='flex-1'>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   )
 }
