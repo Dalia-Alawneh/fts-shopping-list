e48b096dc1b0223cf249568b8fa5dbf701e19f2a
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-22 06:54:21 +0300

    Make footer on the bottom

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-10 bg-gray">
+    <footer className="py-10 bg-gray mt-10 w-full justify-self-end">
       <div className="container mx-auto flex justify-center items-center">
         <h3 className="text-lg font-bold">© 2025 <span
           className='text-primary-green font-extrabold'>Green</span> Forge</h3>
--- src/components/shared/Layout/Layout.jsx
+++ src/components/shared/Layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <>
     <Navbar/>
-    <main>
+    <main className='flex-1'>
       <Outlet/>
     </main>
     <Footer/>
